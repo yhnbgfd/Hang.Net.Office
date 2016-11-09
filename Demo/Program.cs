f6511e70f71a8aaba3e227072b662cfed907dcbf
Hang <yhnbgfd@gmail.com>
2016-11-09 09:47:20 +0800

    重写demo；未完成的ToImage；

---
+++ Demo/Program.cs
@@ -0,0 +1,43 @@
+﻿using Hang.Net.Office.Word;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("========================");
+                Console.WriteLine("i：word文档转化成图片");
+                Console.WriteLine("x：关闭程序");
+                Console.Write("请输入功能码：");
+                var key = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+                TestOffice(key);
+            }
+        }
+
+        static void TestOffice(char key)
+        {
+            Console.WriteLine($">>>>>> Test office : {key}");
+            switch (key)
+            {
+                case 'i':
+                    MsWordUtility.ToImage(@"D:\北京平安力合科技发展股份有限公司\排队机\0-平安力合智能排队管理系统(SOMS520&CQ510R5)对外接口通讯协议说明（新后台修改）.doc", @"C:\Users\yhnbg\Desktop\a");
+                    break;
+                case 'x':
+                    Environment.Exit(0);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
--- Hang.Net.Office/Word/MsWordUtility.cs
+++ Hang.Net.Office/Word/MsWordUtility.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using InteropWord = Microsoft.Office.Interop.Word;
 
 namespace Hang.Net.Office.Word
@@ -44,7 +47,76 @@ namespace Hang.Net.Office.Word
                     app.Quit();
                 }
             }
+        }
 
+        /// <summary>
+        /// Convert doc to image
+        /// </summary>
+        /// <param name="docFile"></param>
+        /// <param name="imagePath"></param>
+        public static void ToImage(string docFile, string imagePath)
+        {
+            string docFileName = docFile.Remove(0, docFile.LastIndexOf("\\") + 1);
+            object wordFile = docFile;
+
+            InteropWord.Application app = null;
+            InteropWord.Document doc = null;
+
+            try
+            {
+                app = new InteropWord.Application();
+                app.Visible = false;
+                app.DisplayAlerts = InteropWord.WdAlertLevel.wdAlertsNone;
+
+                doc = app.Documents.Open(ref wordFile);
+
+                doc.ShowGrammaticalErrors = false;
+                doc.ShowRevisions = false;
+                doc.ShowSpellingErrors = false;
+
+                //Opens the word document and fetch each page and converts to image
+                foreach (InteropWord.Window window in doc.Windows)
+                {
+                    foreach (InteropWord.Pane pane in window.Panes)
+                    {
+                        for (var i = 1; i <= pane.Pages.Count; i++)
+                        {
+                            var page = pane.Pages[i];
+                            var bits = page.EnhMetaFileBits;
+                            var target = Path.Combine(imagePath + "\\" + docFileName.Split('.')[0], string.Format("{1}_page_{0}", i, imagePath.Split('.')[0]));
+
+                            try
+                            {
+                                using (var ms = new MemoryStream((byte[])(bits)))
+                                {
+                                    var image = Image.FromStream(ms);
+                                    var pngTarget = Path.ChangeExtension(target, "png");
+                                    image.Save(pngTarget, ImageFormat.Png);
+                                }
+                            }
+                            catch (Exception ex)
+                            {
+
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (doc != null)
+                {
+                    doc.Close();
+                }
+                if (app != null)
+                {
+                    app.Quit();
+                }
+            }
         }
     }
 }
